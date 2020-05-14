9269123273a1e59d0014510d8a94db902fa1ddec
hayden cordeiro <cordeirohayden@gmail.com>
2020-05-14 05:50:57 -0700

    Updates

---
+++ precache-manifest.8436713e017e4af5e1247b52e1e79502.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d14e7d626a603c605acd4c0095b47eb",
+    "url": "/Xand0/index.html"
+  },
+  {
+    "revision": "29324dadfff79ad41499",
+    "url": "/Xand0/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "442f662a619fb37c7220",
+    "url": "/Xand0/static/js/2.c9eee16a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Xand0/static/js/2.c9eee16a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29324dadfff79ad41499",
+    "url": "/Xand0/static/js/main.5c70830a.chunk.js"
+  },
+  {
+    "revision": "eb1ca07aa47931e6c2ce",
+    "url": "/Xand0/static/js/runtime-main.4360ff5b.js"
+  }
+]);
